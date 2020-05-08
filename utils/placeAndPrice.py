43961b61bb402333d807321a939971fc54aa9ac3
iyujir <48701673+iyujir@users.noreply.github.com>
2020-05-08 20:29:24 +0800

    v0001

---
+++ utils/placeAndPrice.py
@@ -0,0 +1,15 @@
+# CopyRight @yujir 
+# 根据车站和地址生成价格
+
+
+class PlaceAndPriceTools:
+    
+    def __init__(self):
+        pass
+    
+    def enterTo(self,aboardTimes):
+        print(aboardTimes)
+        pass
+
+if __name__ == "__main__":
+    pass
